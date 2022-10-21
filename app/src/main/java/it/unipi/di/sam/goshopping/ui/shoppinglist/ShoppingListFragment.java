082afd016188edd7efe7acf005829c65471fd884
Fabrizio Cau <113622800+caufab@users.noreply.github.com>
2022-10-22 01:47:38 +0200

    added shopping list adapter for cardview (testing)

--- app/src/main/java/it/unipi/di/sam/goshopping/ui/shoppinglist/ShoppingListFragment.java
+++ app/src/main/java/it/unipi/di/sam/goshopping/ui/shoppinglist/ShoppingListFragment.java
@@ -9,12 +9,19 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import it.unipi.di.sam.goshopping.R;
 import it.unipi.di.sam.goshopping.databinding.FragmentShoppinglistBinding;
 
 public class ShoppingListFragment extends Fragment {
 
     private FragmentShoppinglistBinding binding;
+    private List<String> shoppingitems;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,8 +31,25 @@ public class ShoppingListFragment extends Fragment {
         binding = FragmentShoppinglistBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textShoppinglist;
-        ShoppingListViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+    //    final TextView textView = binding.textShoppinglist;
+    //    ShoppingListViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+    //    root.findViewById()
+        RecyclerView rv = (RecyclerView) root.findViewById(R.id.shoppinglist_rv);
+        rv.setHasFixedSize(true);
+
+        LinearLayoutManager llm = new LinearLayoutManager(getContext());
+        rv.setLayoutManager(llm);
+
+        shoppingitems = new ArrayList<>();
+
+        for(int i=0;i<20;i++) {
+            shoppingitems.add("cibo "+i);
+        }
+
+        ShoppingListAdapter adapter = new ShoppingListAdapter(shoppingitems);
+        rv.setAdapter(adapter);
+
         return root;
     }
 
